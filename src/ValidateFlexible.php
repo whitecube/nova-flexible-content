bc37a221f1df63e8b4ee8e7304ac1df74ae528bc
Mikael Popowicz <mikael.popowicz@gmail.com>
2019-05-21 20:08:37 +0200

    :bug: ValidateFlexible use static methods.

--- src/ValidateFlexible.php
+++ src/ValidateFlexible.php
@@ -14,7 +14,7 @@ trait ValidateFlexible
      * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
      * @return \Illuminate\Support\Collection
      */
-    protected function getFlexibleFields(NovaRequest $request): Collection
+    protected static function getFlexibleFields(NovaRequest $request): Collection
     {
         return (new static(new static::$model))->availableFields($request)
             ->filter(function ($field) {
@@ -29,9 +29,9 @@ trait ValidateFlexible
      * @param  \Illuminate\Contracts\Validation\Validator  $validator
      * @return void
      */
-    protected function validateFlexibleFields(NovaRequest $request, ValidatorContract $validator): void
+    protected static function validateFlexibleFields(NovaRequest $request, ValidatorContract $validator): void
     {
-        $this->getFlexibleFields($request)->each(function (Flexible $field) use ($validator, $request) {
+        static::getFlexibleFields($request)->each(function (Flexible $field) use ($validator, $request) {
             $field->validate($request, $validator);
         });
     }
@@ -43,9 +43,9 @@ trait ValidateFlexible
      * @param  \Illuminate\Contracts\Validation\Validator  $validator
      * @return void
      */
-    protected function validateFlexibleFieldsForCreation(NovaRequest $request, ValidatorContract $validator): void
+    protected static function validateFlexibleFieldsForCreation(NovaRequest $request, ValidatorContract $validator): void
     {
-        $this->getFlexibleFields($request)->each(function (Flexible $field) use ($validator, $request) {
+        static::getFlexibleFields($request)->each(function (Flexible $field) use ($validator, $request) {
                 $field->validateForCreation($request, $validator);
             });
     }
@@ -57,9 +57,9 @@ trait ValidateFlexible
      * @param  \Illuminate\Contracts\Validation\Validator  $validator
      * @return void
      */
-    protected function validateFlexibleFieldsForUpdate(NovaRequest $request, ValidatorContract $validator): void
+    protected static function validateFlexibleFieldsForUpdate(NovaRequest $request, ValidatorContract $validator): void
     {
-        $this->getFlexibleFields($request)->each(function (Flexible $field) use ($validator, $request) {
+        static::getFlexibleFields($request)->each(function (Flexible $field) use ($validator, $request) {
             $field->validateForUpdate($request, $validator);
         });
     }
