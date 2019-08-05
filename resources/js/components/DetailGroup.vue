b8bfcb77a9b82bd4fe6a4d20a785b5af0cda6dd3
Toon Van den Bos <toon@whitecube.be>
2019-08-05 16:20:46 +0200

    Removed empty titles from DetailGroup component. Closes #50

--- resources/js/components/DetailGroup.vue
+++ resources/js/components/DetailGroup.vue
@@ -1,25 +1,23 @@
 <template>
-    <div :class="classes">
-        <div class="pb-4 border-b border-40">
+    <div :class="componentStyle">
+        <div :class="titleStyle" v-if="group.title">
             <span class="block float-left border-r border-40 pr-4 mr-4"><!--
              --><span class="text-60 text-xs">#</span><!--
              --><span class="text-80">{{index+1}}</span>
             </span>
             <span class="font-bold">{{group.title}}</span>
         </div>
-        <div :class="last ? 'pt-6' : 'py-6'">
-            <component
-                v-for="(item, index) in group.fields"
-                :key="index"
-                :is="'detail-' + item.component"
-                :resource-name="resourceName"
-                :resource-id="resourceId"
-                :resource="resource"
-                :field="item"
-                :errors="validationErrors"
-                :class="{ 'remove-bottom-border': index == group.fields.length - 1 }"
-            />
-        </div>
+        <component
+            v-for="(item, index) in group.fields"
+            :key="index"
+            :is="'detail-' + item.component"
+            :resource-name="resourceName"
+            :resource-id="resourceId"
+            :resource="resource"
+            :field="item"
+            :errors="validationErrors"
+            :class="{ 'remove-bottom-border': index == group.fields.length - 1 }"
+        />
     </div>
 </template>
 
@@ -28,9 +26,12 @@ export default {
     props: ['group', 'index', 'last'],
 
     computed: {
-        classes() {
-            return this.last ? '' : 'border-b border-40 mb-4'
+        componentStyle() {
+            return this.last ? [] : ['border-b border-50 pb-4 mb-4'];
         },
+        titleStyle() {
+            return ['pb-4', 'border-b', 'border-40'];
+        }
     }
 }
 </script>
