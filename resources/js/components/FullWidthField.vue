1ea345d7b66b93af0127a568144a46c3698c6d0c
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2022-05-14 23:03:44 +0300

    Update FullWidthField.vue

--- resources/js/components/FullWidthField.vue
+++ resources/js/components/FullWidthField.vue
@@ -1,5 +1,5 @@
 <template>
-    <field-wrapper>
+    <field-wrapper v-if="field.visible">
         <div class="py-6 px-8 w-full">
             <div class="mb-6" v-if="fieldLabel">
                 <form-label :for="field.attribute" :class="{
