cd691589a9bdf84a812b49ead43da829f6050f83
ianrobertsFF <91917328+ianrobertsFF@users.noreply.github.com>
2022-05-23 21:25:06 +0100

    fix: FullWidthField helptext component
    -Full Width Field was using the wrong help text component name
    - It also passed help text as text not html
    - Aligned with nova components and props

--- resources/js/components/FullWidthField.vue
+++ resources/js/components/FullWidthField.vue
@@ -1,57 +1,59 @@
 <template>
-    <field-wrapper>
-        <div class="py-6 px-8 w-full">
-            <div class="mb-6" v-if="fieldLabel">
-                <form-label :for="field.attribute" :class="{
-                      'mb-2': field.helpText && showHelpText
-                  }">
-                    {{ fieldLabel }}
+  <field-wrapper>
+    <div class="py-6 px-8 w-full">
+      <div class="mb-6" v-if="fieldLabel">
+        <form-label
+          :for="field.attribute"
+          :class="{
+            'mb-2': field.helpText && showHelpText,
+          }"
+        >
+          {{ fieldLabel }}
 
-                    <span v-if="field.required" class="text-danger text-sm">{{
-                        __('*')
-                    }}</span>
-                </form-label>
+          <span v-if="field.required" class="text-danger text-sm">{{
+            __("*")
+          }}</span>
+        </form-label>
 
-                <help-text v-if="showHelpText">
-                    {{ field.helpText }}
-                </help-text>
-            </div>
+        <HelpText
+          class="help-text mt-2"
+          v-if="showHelpText"
+          v-html="field.helpText"
+        />
+      </div>
 
-            <slot name="field"/>
+      <slot name="field" />
 
-            <help-text
-                class="error-text mt-2 text-danger"
-                v-if="showErrors && hasError"
-            >
-                {{ firstError }}
-            </help-text>
-        </div>
-    </field-wrapper>
+      <HelpText class="mt-2 help-text-error" v-if="showErrors && hasError">
+        {{ firstError }}
+      </HelpText>
+    </div>
+  </field-wrapper>
 </template>
 
 <script>
-import { mapProps } from 'laravel-nova'
-import { HandlesValidationErrors, Errors } from 'laravel-nova'
+import { mapProps } from "laravel-nova";
+import { HandlesValidationErrors, Errors } from "laravel-nova";
 
 export default {
-    mixins: [HandlesValidationErrors],
+  mixins: [HandlesValidationErrors],
 
-    props: {
-        field: { type: Object, required: true },
-        fieldName: { type: String },
-        showErrors: { type: Boolean, default: true },
-        ...mapProps(['showHelpText']),
+  props: {
+    field: { type: Object, required: true },
+    fieldName: { type: String },
+    showErrors: { type: Boolean, default: true },
+    ...mapProps(["showHelpText"]),
+  },
+
+  computed: {
+    fieldLabel() {
+      // If the field name is purposefully empty, hide the label altogether
+      if (this.fieldName === "") {
+        return false;
+      }
+
+      return this.fieldName || this.field.singularLabel || this.field.name;
     },
-
-    computed: {
-        fieldLabel() {
-            // If the field name is purposefully empty, hide the label altogether
-            if (this.fieldName === '') {
-                return false;
-            }
-
-            return this.fieldName || this.field.singularLabel || this.field.name
-        },
-    },
+  },
 };
 </script>
