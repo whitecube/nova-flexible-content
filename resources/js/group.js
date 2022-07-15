131ed8192b274ec8fa31173309e65227297dba57
Adrien Leloup <adrien@whitecube.be>
2022-07-15 15:01:32 +0200

    Formatting

--- resources/js/group.js
+++ resources/js/group.js
@@ -80,11 +80,12 @@ export default class Group {
         for (var i = this.fields.length - 1; i >= 0; i--) {
             this.fields[i].attribute = this.key + '__' + this.fields[i].attribute;
             this.fields[i].validationKey = this.fields[i].attribute;
+
             if (this.fields[i].dependsOn) {
                 Object.keys(this.fields[i].dependsOn).forEach(key => {
-                    this.fields[i].dependsOn[`${this.key}__${key}`] = this.fields[i].dependsOn[key]
-                    delete this.fields[i].dependsOn[key]
-                })
+                    this.fields[i].dependsOn[`${this.key}__${key}`] = this.fields[i].dependsOn[key];
+                    delete this.fields[i].dependsOn[key];
+                });
             }
         }
     }
