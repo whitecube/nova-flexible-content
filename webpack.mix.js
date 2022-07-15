07667de5b04e98e6d74ea76508557dd8458f151a
Adrien Leloup <adrien@whitecube.be>
2022-07-15 14:38:09 +0200

    Revert NovaExtension

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,20 @@
 let mix = require('laravel-mix')
 let path = require('path')
 
-require('./nova.mix')
-
 mix
     .setPublicPath('dist')
     .js('resources/js/field.js', 'js')
     .vue({ version: 3 })
     .sass('resources/sass/field.scss', 'css')
-    .nova('whitecube/nova-flexible-content')
+    .alias({
+        'laravel-nova': path.join(__dirname, './vendor/laravel/nova/resources/js/mixins/packages.js'),
+        'nova-mixins': path.join(__dirname,'./vendor/laravel/nova/resources/js/mixins')
+    })
+    .webpackConfig({
+        externals: {
+            vue: 'Vue',
+        },
+        output: {
+            uniqueName: 'whitecube/nova-flexible-content',
+        },
+    });
