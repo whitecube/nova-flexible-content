552841a2c08d9f3e80e018e2c05f8ce8f4887d29
John Puddephatt <john@jdp.org.uk>
2023-03-29 23:24:23 +0100

    work with nova fields with storage callbacks

--- resources/js/components/FieldListWithPreview.vue
+++ resources/js/components/FieldListWithPreview.vue
@@ -1,46 +1,85 @@
 <template>
     <div>
-        
-        <div @click="$emit('group-selected')" class="relative" :class="{'pointer-events-none': selectedGroup && !initialPreviewHtml}" style="min-height: 80px;" >
-            <div v-if="fieldListErrorCount" class="font-bold z-20 mt-3 ml-3 border shadow absolute bg-white px-4 py-2 rounded">
-                <icon type="exclamation-circle" class="text-red-600 align-middle" width="20" height="20" />
-                {{fieldListErrorCount }} errors
+        <div
+            @click="$emit('group-selected')"
+            class="relative"
+            :class="{
+                'pointer-events-none': selectedGroup && !initialPreviewHtml,
+            }"
+            style="min-height: 80px"
+        >
+            <div
+                v-if="fieldListErrorCount"
+                class="font-bold z-20 mt-3 ml-3 border shadow absolute bg-white px-4 py-2 rounded"
+            >
+                <icon
+                    type="exclamation-circle"
+                    class="text-red-600 align-middle"
+                    width="20"
+                    height="20"
+                />
+                {{ fieldListErrorCount }} errors
             </div>
-            <div class="absolute  z-10 inset-0 transition border-2" :class="{ 'pointer-events-none  border-primary-500 border-dashed' : selectedGroup, 'hover:border-gray-200 border-transparent' : !selectedGroup }"></div>
-            <preview-iframe v-if="initialPreviewHtml" :stylesheet="stylesheet" :flexible_key="flexible_key" :fullScreen="fullScreen" :initialPreviewHtml="initialPreviewHtml" :updatedPreviewHtml="updatedPreviewHtml" />
+            <div
+                class="absolute z-10 inset-0 transition border-2"
+                :class="{
+                    'pointer-events-none  border-primary-500 border-dashed':
+                        selectedGroup,
+                    'hover:border-gray-200 border-transparent': !selectedGroup,
+                }"
+            ></div>
+            <preview-iframe
+                v-if="initialPreviewHtml"
+                :stylesheet="stylesheet"
+                :flexible_key="flexible_key"
+                :fullScreen="fullScreen"
+                :initialPreviewHtml="initialPreviewHtml"
+                :updatedPreviewHtml="updatedPreviewHtml"
+            />
         </div>
-         <div v-show="selectedGroup"
-            class="absolute pt-4 top-0 left-0 md:w-1/5 bottom-0 h-full  bg-gray-50 overflow-y-scroll self-stretch">
-           
+        <div
+            v-show="selectedGroup"
+            class="absolute pt-4 top-0 left-0 md:w-1/5 bottom-0 h-full bg-gray-50 overflow-y-scroll self-stretch"
+        >
             <div class="w-full py-5 overflow-hidden">
-                <div class="px-6 py-6 gap-2 flex flex-row items-center ">
-                    <button :aria-label="`Close ${ title }`" @click.prevent="$emit('group-selected')">    
-                        <icon type="arrow-left" class="align-top rounded-full hover:bg-gray-200 p-2" width="36" height="36" />
+                <div class="px-6 py-6 gap-2 flex flex-row items-center">
+                    <button
+                        :aria-label="`Close ${title}`"
+                        @click.prevent="$emit('group-selected')"
+                    >
+                        <icon
+                            type="arrow-left"
+                            class="align-top rounded-full hover:bg-gray-200 p-2"
+                            width="36"
+                            height="36"
+                        />
                     </button>
-                    <h3 class="font-semibold">{{  title }}</h3>
+                    <h3 class="font-semibold">{{ title }}</h3>
                 </div>
 
                 <div
                     class="fields relative divide-y divide-gray-100 dark:divide-gray-700"
-                    style="margin-left: -0.25rem; margin-right: -0.25rem;">
+                    style="margin-left: -0.25rem; margin-right: -0.25rem"
+                >
                     <fieldset ref="fieldset">
-                    <component
-                        v-for="(item, index) in stackedFields"
-                        :key="index"
-                        :is="'form-' + item.component"
-                        :resource-name="resourceName"
-                        :resource-id="resourceId"
-                        fullWidthContent="true"
-                        @input="onInput($event, item)"
-                        @change="onInput($event, item)"
-                        :field="item"
-                        :errors="errors"
-                        :mode="mode"
-                        :show-help-text="item.helpText != null"
-                        :class="{
-                            'remove-bottom-border': index == fields.length - 1,
-                        }"
-                    />
+                        <component
+                            v-for="(item, index) in stackedFields"
+                            :key="index"
+                            :is="'form-' + item.component"
+                            :resource-name="resourceName"
+                            :resource-id="resourceId"
+                            fullWidthContent="true"
+                            @input="onInput($event, item)"
+                            @change="onInput($event, item)"
+                            :field="item"
+                            :errors="errors"
+                            :mode="mode"
+                            :show-help-text="item.helpText != null"
+                            :class="{
+                                'remove-bottom-border':
+                                    index == fields.length - 1,
+                            }"
+                        />
                     </fieldset>
                 </div>
             </div>
@@ -49,9 +88,9 @@
 </template>
 
 <script>
-import { tsImportEqualsDeclaration } from '@babel/types';
-import _, { map } from 'underscore';
-import PreviewIframe from './PreviewIframe';
+import { tsImportEqualsDeclaration } from "@babel/types";
+import _, { map } from "underscore";
+import PreviewIframe from "./PreviewIframe";
 
 export default {
     props: {
@@ -69,14 +108,14 @@ export default {
         title: null,
     },
 
-    components: {PreviewIframe},
+    components: { PreviewIframe },
 
     data() {
         return {
             form: new FormData(),
             initialPreviewHtml: null,
             updatedPreviewHtml: null,
-            containsInvalidFormElements: false
+            containsInvalidFormElements: false,
         };
     },
 
@@ -89,8 +128,10 @@ export default {
         },
 
         fieldListErrorCount() {
-            return this.fields.filter((field) => Object.keys(this.errors.errors).includes(field.attribute)).length 
-        }
+            return this.fields.filter((field) =>
+                Object.keys(this.errors.errors).includes(field.attribute)
+            ).length;
+        },
     },
 
     mounted() {
@@ -98,40 +139,37 @@ export default {
 
         this.$nextTick(() => {
             this.fields.forEach((field) => {
-                this.form.set(field.attribute, field.value ?? '');
-                    field.fill(this.form);                
+                this.form.set(field.attribute, field.value ?? "");
+                field.fill(this.form);
             });
             this.getPreview();
         });
-
     },
 
     watch: {
         selectedGroup(newValue, oldValue) {
-            if(!newValue) {
+            if (!newValue) {
                 this.validateFields();
-                if(this.containsInvalidFormElements) {
-                    this.$emit('group-selected');
-                    this.$refs.fieldset.elements.forEach(field => {
+                if (this.containsInvalidFormElements) {
+                    this.$emit("group-selected");
+                    this.$refs.fieldset.elements.forEach((field) => {
                         field.reportValidity();
-                    })
+                    });
                 }
             }
-        }
-
+        },
     },
 
     methods: {
-
-        validateFields() {            
+        validateFields() {
             this.containsInvalidFormElements = false;
-            this.$refs.fieldset.elements.forEach(field => {
-                if(!field.checkValidity()) {
+            this.$refs.fieldset.elements.forEach((field) => {
+                if (!field.checkValidity()) {
                     this.containsInvalidFormElements = true;
                 }
             });
         },
-        
+
         onInput: _.debounce(function debounceRead(evt, updatedField) {
             this.update(updatedField);
         }, 150),
@@ -150,7 +188,7 @@ export default {
                 `/nova-vendor/flexible/view/${this.resourceName}/${this.resourceId}/${this.fieldName}/${this.layoutName}`,
                 {
                     method: "post",
-                    body: this.form,
+                    body: JSON.stringify(this.form),
                     headers: {
                         "X-CSRF-TOKEN": document.querySelector(
                             'meta[name="csrf-token"]'
@@ -160,18 +198,19 @@ export default {
             )
                 .then((response) => response.json())
                 .then((json) => {
-
                     // Reset the form so we don't keep uploading files on every refresh
                     this.form = new FormData();
                     this.form.append("__key", this.flexible_key);
                     this.fields.forEach((field) => {
-                        this.form.set(field.attribute, json.data[field.attribute] ?? '');
+                        this.form.set(
+                            field.attribute,
+                            JSON.parse(json.data[field.attribute] ?? "")
+                        );
                     });
 
-                    if(!this.initialPreviewHtml || json.has_uploads) {
+                    if (!this.initialPreviewHtml || json.has_uploads) {
                         this.initialPreviewHtml = json.view;
-                    }
-                    else {
+                    } else {
                         this.updatedPreviewHtml = json.view;
                     }
                 });
@@ -219,11 +258,9 @@ export default {
 .fields .md\:w-1\/4,
 .fields .w-1\/2,
 .fields .md\:w-4\/5,
-.fields .md\:w-3\/5  {
-
-    width: 100%
+.fields .md\:w-3\/5 {
+    width: 100%;
 }
-
 
 .nsr-w-full.nsr-flex.nsr-border-b.nsr-py-2 {
     display: none;
@@ -245,11 +282,21 @@ export default {
     width: 80% !important;
 } */
 
-.fields .simple-repeatable.form-field .simple-repeatable-row .delete-icon, .simple-repeatable.form-field .simple-repeatable-row .vue-draggable-handle {
+.fields .simple-repeatable.form-field .simple-repeatable-row .delete-icon,
+.simple-repeatable.form-field .simple-repeatable-row .vue-draggable-handle {
     margin-right: 0;
 }
 
-.simple-repeatable.form-field .simple-repeatable-row>.simple-repeatable-fields-wrapper .translatable-field>div:not(:first-child)>div, .simple-repeatable.form-field .simple-repeatable-row>.simple-repeatable-fields-wrapper>* {
+.simple-repeatable.form-field
+    .simple-repeatable-row
+    > .simple-repeatable-fields-wrapper
+    .translatable-field
+    > div:not(:first-child)
+    > div,
+.simple-repeatable.form-field
+    .simple-repeatable-row
+    > .simple-repeatable-fields-wrapper
+    > * {
     margin-right: 0;
 }
 
@@ -257,4 +304,3 @@ export default {
     border-color: rgba(var(--colors-gray-200));
 }
 </style>
-
--- src/Http/Controllers/ViewController.php
+++ src/Http/Controllers/ViewController.php
@@ -5,6 +5,7 @@ namespace Whitecube\NovaFlexibleContent\Http\Controllers;
 use Illuminate\Support\Facades\Storage;
 use Laravel\Nova\Http\Controllers\ActionController as NovaActionController;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Illuminate\Support\Arr;
 
 class ViewController extends NovaActionController
 {
@@ -15,18 +16,17 @@ class ViewController extends NovaActionController
         $fieldName,
         $layoutName
     ) {
-
         // No idea if this is right
         $novaResource = $request->resource();
         $novaResource::authorizeToCreate($request);
-        
+
         // Find the layout. Is there a better way?
         $layout = $request
             ->findResource($resourceId)
             ->resolveFieldForAttribute($request, urldecode($fieldName))
-            ->meta['layouts']->find($layoutName);
+            ->meta["layouts"]->find($layoutName);
 
-        // Not sure if there's a way of finding the layout that does this automatically. 
+        // Not sure if there's a way of finding the layout that does this automatically.
         // Needed to give us access to $layout->model, e.g. in accessors.
         $layout->setModel($request->findModel($resourceId));
 
@@ -40,28 +40,55 @@ class ViewController extends NovaActionController
         //         }
         //     }
         // }
-    
+
         // Set attributes on our layout
-        foreach($this->removeKeyPrefixFromFields($values, $request->__key) as $key => $value) {
-            if(is_array($value)) {
+        foreach (
+            $this->removeKeyPrefixFromFields($values, $request->__key)
+            as $key => $value
+        ) {
+            $field = Arr::first($layout->fields(), function ($field) use (
+                $key
+            ) {
+                return $field->attribute == $key;
+            });
+
+            if (
+                get_class($field) == "Laravel\Nova\Fields\Image" &&
+                $request->file($request->__key . "__" . $key)
+            ) {
+
+                $layout->setAttribute(
+                    $key,
+                    json_encode(
+                        $field->storageCallback->__invoke(
+                            $request,
+                            $layout,
+                            $key,
+                            $request->__key . "__" . $key,
+                            null,
+                            "temporary_uploads/"
+                        )[$key]
+                    )
+                );
+            } elseif (is_array($value)) {
                 $layout->setAttribute($key, json_encode($value));
-            }
-            else {
+            } else {
                 $layout->setAttribute($key, json_decode($value) ?? $value); // json_decode needed for simple repeater field
             }
         }
-        
-        return response()->json([
-            'view' => view('flexible.' . $layoutName, ['layout' => $layout])->render(),
-            'data' =>  $values,
-            'has_uploads' => !!count($request->files)
-        ]);
 
+        return response()->json([
+            "view" => view("flexible." . $layoutName, [
+                "layout" => $layout,
+            ])->render(),
+            "data" => $values,
+            "has_uploads" => !!count($request->files),
+        ]);
     }
 
     private function removeKeyPrefixFromFields($array, $key)
     {
-        $unprefixed_keys = str_replace($key.'__', '', array_keys($array));
+        $unprefixed_keys = str_replace($key . "__", "", array_keys($array));
         return array_combine($unprefixed_keys, $array);
     }
 }
