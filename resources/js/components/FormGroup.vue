4a4b7a6fd74c1679446d96a8f8eca6ce87d165e7
Fayne Guo <fayne.guo@gmail.com>
2025-06-20 13:31:16 +0800

    :+1: The title field can be used more flexibly

--- resources/js/components/FormGroup.vue
+++ resources/js/components/FormGroup.vue
@@ -37,7 +37,7 @@
 
           <p class="text-80 grow px-4">
             <span class="mr-3 font-semibold">#{{ index + 1 }}</span>
-            {{ group.title }}
+            <span v-html="group.title"></span>
           </p>
 
           <div class="flex" v-if="!readonly">
